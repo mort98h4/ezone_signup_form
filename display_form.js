5c41f7f0d5458572b92174bd5e52f5215c885863
Morten Gross <mg@vuffeli.dk>
2021-04-26 11:18:31 +0200

    checkbox value moved to the switch statement

--- display_form.js
+++ display_form.js
@@ -22,7 +22,6 @@ function displayForm(destination, jsonData, nameAtr) {
         const checkBox = document.createElement("input");
         checkBox.setAttribute("type", "checkbox");
         checkBox.setAttribute("name", nameAtr);
-        checkBox.value = newEntry;
 
         label.append(checkBox, entry);
 
@@ -30,14 +29,17 @@ function displayForm(destination, jsonData, nameAtr) {
             case "problem_area":
                 label.setAttribute("for", `${newEntry}1`);
                 checkBox.setAttribute("id", `${newEntry}1`);
+                checkBox.value = `${newEntry}1`;
                 break;
             case "improvement_area":
                 label.setAttribute("for", `${newEntry}2`);
                 checkBox.setAttribute("id", `${newEntry}2`);
+                checkBox.value = `${newEntry}2`;
                 break;
             default: 
                 label.setAttribute("for", newEntry);
                 checkBox.setAttribute("id", newEntry);
+                checkBox.value = newEntry;
         }   
     });
 }
