0239169da3c95420fe8ce270ffe9056c75ac3a8d
Morten Gross <mg@vuffeli.dk>
2021-04-26 10:47:20 +0200

    displaying the part of the form of game types is working

--- display_form.js
+++ display_form.js
@@ -2,4 +2,30 @@
 
 export function formController(gameData) {
     console.log(gameData);
+
+    displayForm("#gameTypes", gameData.types, "game_type");
+}
+
+function displayForm(destination, jsonData, nameAtr) {
+    console.log(destination, jsonData);
+    const dest = document.querySelector(destination);
+
+    jsonData.forEach(entry => {
+        let newEntry = entry.toLowerCase().replace(" ", "-");
+        
+        const li = document.createElement("li");
+        dest.append(li);
+
+        const label = document.createElement("label");
+        label.setAttribute("for", newEntry);
+        li.append(label); 
+
+        const checkBox = document.createElement("input");
+        checkBox.setAttribute("type", "checkbox");
+        checkBox.setAttribute("id", newEntry);
+        checkBox.setAttribute("name", nameAtr);
+        checkBox.value = newEntry;
+
+        label.append(checkBox, entry);
+    });
 }
