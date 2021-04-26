2e6002e2e260b308e6c88ff8406c8bcc80f1ce90
Morten Gross <mg@vuffeli.dk>
2021-04-26 11:14:54 +0200

    the rest of the form generated from the json data is now added

--- display_form.js
+++ display_form.js
@@ -4,6 +4,9 @@ export function formController(gameData) {
     console.log(gameData);
 
     displayForm("#gameTypes", gameData.types, "game_type");
+    displayForm("#games", gameData.games, "games");
+    displayForm("#problemAreas", gameData.areas, "problem_area");
+    displayForm("#improvementAreas", gameData.areas, "improvement_area");
 }
 
 function displayForm(destination, jsonData, nameAtr) {
@@ -11,21 +14,34 @@ function displayForm(destination, jsonData, nameAtr) {
     const dest = document.querySelector(destination);
 
     jsonData.forEach(entry => {
-        let newEntry = entry.toLowerCase().replace(" ", "-");
+        const newEntry = entry.toLowerCase().replaceAll(" ", "-").replaceAll("/", "-");
+        console.log(newEntry)
         
         const li = document.createElement("li");
         dest.append(li);
 
         const label = document.createElement("label");
-        label.setAttribute("for", newEntry);
         li.append(label); 
 
         const checkBox = document.createElement("input");
         checkBox.setAttribute("type", "checkbox");
-        checkBox.setAttribute("id", newEntry);
         checkBox.setAttribute("name", nameAtr);
         checkBox.value = newEntry;
 
         label.append(checkBox, entry);
+
+        switch (nameAtr) {
+            case "problem_area":
+                label.setAttribute("for", `${newEntry}1`);
+                checkBox.setAttribute("id", `${newEntry}1`);
+                break;
+            case "improvement_area":
+                label.setAttribute("for", `${newEntry}2`);
+                checkBox.setAttribute("id", `${newEntry}2`);
+                break;
+            default: 
+                label.setAttribute("for", newEntry);
+                checkBox.setAttribute("id", newEntry);
+        }   
     });
 }
