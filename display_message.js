f39caed6e322f36b3b886d5d6c58a25a0a0623bc
Morten Gross <mg@vuffeli.dk>
2021-04-27 18:10:31 +0200

    Paused this. But switch is ready

--- display_message.js
+++ display_message.js
@@ -2,5 +2,12 @@
 
 export function displayEndMessage(status) {
     console.log("displayEndMessage", status);
-
+    
+    switch (status) {
+        case 201:
+            document.querySelector("form").setAttribute("hidden", true);
+            break;
+        default: 
+            console.log("Something went terribly wrong...")
+    }  
 }
