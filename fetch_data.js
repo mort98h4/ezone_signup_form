03a26aaf4f2a93d7220bca76ce30ac4d0b5b357b
Morten Gross <mg@vuffeli.dk>
2021-04-27 09:55:42 +0200

    file is not needed anymore

--- fetch_data.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-export async function fetchJson() {
-    const jsonData = await fetch("./public/data.json");
-    const gamingData = await jsonData.json();
-
-    return gamingData;
-}
