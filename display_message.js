53fa1a4c39ab4951ca6a0b788cc7fa16e73ae892
Morten Gross <mg@vuffeli.dk>
2021-04-28 19:43:40 +0200

    edited the succes message

--- display_message.js
+++ display_message.js
@@ -6,8 +6,8 @@ export function displayEndMessage(status) {
   switch (status) {
     case 201:
       document.querySelector("form").setAttribute("hidden", true);
-      document.querySelector(".message").textContent =
-        "Thank you for signing up! Log in and read our suggestions of articles for you";
+      document.querySelector(".message").innerHTML =
+        "Thank you for signing up! <br>Log in and read our suggestions of articles for you.";
       document.querySelector(".view_form").setAttribute("hidden", true);
       break;
     default:
