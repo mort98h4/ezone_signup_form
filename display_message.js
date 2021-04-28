0845986eb80e371c5f3a07d3f7348323d8662ce6
Morten Gross <mg@vuffeli.dk>
2021-04-28 10:57:02 +0200

    Now has a post succeeded message

--- display_message.js
+++ display_message.js
@@ -6,6 +6,8 @@ export function displayEndMessage(status) {
     switch (status) {
         case 201:
             document.querySelector("form").setAttribute("hidden", true);
+            document.querySelector(".message").textContent = "Thank you for signing up!";
+            document.querySelector(".view_form").setAttribute("hidden", true);
             break;
         default: 
             console.log("Something went terribly wrong...")
