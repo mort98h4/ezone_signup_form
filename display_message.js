04e488cdf95ef8f1dc91115974b64d9a7a84cbaf
Siw <siwx0310@stud.kea.dk>
2021-04-28 18:56:33 +0200

    styling complete

--- display_message.js
+++ display_message.js
@@ -1,15 +1,16 @@
 "use strict";
 
 export function displayEndMessage(status) {
-    console.log("displayEndMessage", status);
-    
-    switch (status) {
-        case 201:
-            document.querySelector("form").setAttribute("hidden", true);
-            document.querySelector(".message").textContent = "Thank you for signing up!";
-            document.querySelector(".view_form").setAttribute("hidden", true);
-            break;
-        default: 
-            console.log("Something went terribly wrong...")
-    }  
+  console.log("displayEndMessage", status);
+
+  switch (status) {
+    case 201:
+      document.querySelector("form").setAttribute("hidden", true);
+      document.querySelector(".message").textContent =
+        "Thank you for signing up! Log in and read our suggestions of articles for you";
+      document.querySelector(".view_form").setAttribute("hidden", true);
+      break;
+    default:
+      console.log("Something went terribly wrong...");
+  }
 }
